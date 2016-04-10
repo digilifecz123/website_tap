6cc98750d562264b56e9288349cdd5aa5d8620c0
digilifecz123 <digilifecz123@gmail.com>
2016-04-10 20:17:47 +0200

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,14 @@
+
+    
+
+
+    
+ // moving screenshot hover header
+  //$('.screenshot-container').mouseover(function(){
+    //$(this).animate({ "left": "-=50px" }, "slow" );
+  //});
+
+
+    // slider
+   
+
