86a4b1c04d08483c6b2272c243036cf8d7b9c935
digilifecz123 <digilifecz123@gmail.com>
2016-04-12 01:28:00 +0200

    reoranize

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,14 +1,38 @@
-
+   $('.nav').scrollToFixed();
+        $('.bxslider').bxSlider({
+                 auto: true
+             });
+           
+           
+            $('#toggle').click(function() {
+       $(this).toggleClass('active');
+       $('#overlay').toggleClass('open');
+      });
+           
+           
+           $(window).scroll(function(){
+               
+        var wScroll = $(this).scrollTop();
+      $('.header-container').css({
+        'transform' : 'translate(0px, '+ wScroll +'px)'
+      });
+      $('.topnav').css({
+          'transform' : 'translate(0px, '+ wScroll +'px)'
+      });
+});
+           
+           
+$(document).scroll(function() {
+var y = $(this).scrollTop();
+  if (y > 800) {
+      $(".button_container").hide();
+} else if($(window).width() < 1030) {
+    $(".button_container").show();
     
+}
 
 
-    
- // moving screenshot hover header
-  //$('.screenshot-container').mouseover(function(){
-    //$(this).animate({ "left": "-=50px" }, "slow" );
-  //});
+});
 
-
-    // slider
    
 
